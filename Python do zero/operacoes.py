421136a7e06ea00049787dd04667a6b755915e1f
Hernane Carvalho <hernane.carvalho@tm2digital.com>
2020-11-13 15:39:38 -0300

    operadores

--- Python do zero/operacoes.py
+++ Python do zero/operacoes.py
@@ -7,7 +7,7 @@ status = True
 valor_pastel1 = 6.0
 valor_pastel2 = 6.0
 valor_pastel3 = 5.5
-valor_pastel4 = 6
+valor_pastel4 = 7
 print(nome_estabelecimento)
 print(pastel1,valor_pastel1,status)
 print(pastel2,valor_pastel2,status)
@@ -23,6 +23,16 @@ pedido1= (valor_pastel1+valor_pastel3)
 print ('Valor pedido 1:',pedido1,type(pedido1))
 print('-----------')
 print('-----------')
+pedido2= (valor_pastel2+valor_pastel4)
+print ('Valor pedido 2:',pedido2,type(pedido2))
+print('-----------')
+print('-----------')
+#subtração
 desconto=3.0
 subtracao1=(pedido1-desconto)
 print('Valor com desconto:',subtracao1)
+print('-----------')
+print('-----------')
+#Divisão
+ticket_medio=(pedido1 + pedido2) / 2
+print('O Valor do ticket médio é:',ticket_medio)
