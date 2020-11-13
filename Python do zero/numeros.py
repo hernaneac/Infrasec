e6286a383a0f54ddbc2ab3c21bcdc5c3545d3840
Hernane Carvalho <hernane.carvalho@tm2digital.com>
2020-11-13 15:14:54 -0300

    numeros e funções

---
+++ Python do zero/numeros.py
@@ -0,0 +1,31 @@
+print(4)
+print (4,type(4))
+print('-------')
+print ('4.7')
+print ('4.7',type('4.7'))
+
+print('===============================')
+print('Numeros a partir de float e int')
+print('===============================')
+
+preco_igrediente= 14.70
+print('Preço do ingrediente float', preco_igrediente,type(preco_igrediente))
+print('Preço do ingrediente int', int(preco_igrediente),type(int(preco_igrediente)))
+
+print('===============================',end=('\n\n\n'))
+
+quantidade_estoque=54
+print('Quantidade estoque int',quantidade_estoque,type(quantidade_estoque))
+print('Quantidade estoque Float',float(quantidade_estoque),type(float(quantidade_estoque)))
+
+print('')
+faturamento='10_000'
+print('faturamento string',faturamento,type(faturamento))
+print('faturamento int',int(faturamento),type(int(faturamento)))
+print('faturamento float',float(faturamento),type(float(faturamento)))
+print('')
+print('')
+valor_imposto='2.90'
+print('Valor do impostro string',valor_imposto,type(valor_imposto))
+print('Valor do impostro float',float(valor_imposto),type(float(valor_imposto)))
+print('Valor do impostro int',int(float(valor_imposto)),type(int(float(valor_imposto))))
