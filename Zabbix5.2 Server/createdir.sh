5e4b33a8c0b73fc5186a5333f655d625f84c291f
Hernane Carvalho <hernane.carvalho@tm2digital.com>
2020-12-13 13:45:11 -0300

    zbx server

---
+++ Zabbix5.2 Server/createdir.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+mkdir -p .zbx_env/usr/lib/zabbix/alertscripts
+mkdir -p .zbx_env/usr/lib/zabbix/externalscripts
+mkdir -p .zbx_env/var/lib/zabbix/export
+mkdir -p .zbx_env/var/lib/zabbix/modules
+mkdir -p .zbx_env/var/lib/zabbix/enc
+mkdir -p .zbx_env/var/lib/zabbix/ssh_keys
+mkdir -p .zbx_env/var/lib/zabbix/mibs
+mkdir -p snmptraps
+mkdir -p .zbx_env/etc/ssl/apache2
+mkdir -p .zbx_env/usr/share/zabbix/modules
+mkdir -p .zbx_env/etc/zabbix/zabbix_agentd.d
+mkdir -p .zbx_env/var/lib/zabbix/modules
+mkdir -p .zbx_env/var/lib/zabbix/enc
+mkdir -p .zbx_env/var/lib/zabbix/ssh_keys
+mkdir -p .zbx_env/var/lib/mysql
+
