1aea04fae0f411d0e284bf4adbcad163220f650b
Hernane Carvalho <hernane.carvalho@tm2digital.com>
2021-01-04 20:08:19 -0300

    recurso for

---
+++ Python do zero/for.py
@@ -0,0 +1,14 @@
+cardapio =['Carne', 'Queijo', 'Calabresa', 'Frango', 'Carne Seca']
+
+'''
+print(cardapio[0])
+print(cardapio[2])
+print(cardapio[4])
+'''
+
+print('Pastelaria Devops')
+print('Escolha seu recheio')
+print('---------------')
+for recheio in cardapio:
+    print(f'O recheio do pastel é de: {recheio}')
+
